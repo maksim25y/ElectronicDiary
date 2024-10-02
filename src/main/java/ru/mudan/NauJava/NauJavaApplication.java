5ecfd0062f9f08a81a37408c89523735e77003dc
mudan <myudanov223@gmail.com>
2024-10-02 11:52:29 +0300

    Initial commit

---
+++ src/main/java/ru/mudan/NauJava/NauJavaApplication.java
@@ -0,0 +1,12 @@
+package ru.mudan.NauJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NauJavaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NauJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mudan/NauJava/NauJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mudan.NauJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NauJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
